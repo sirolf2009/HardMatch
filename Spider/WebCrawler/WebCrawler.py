c189a8aed6ccf340eefa8eef36d13643d31ccc05
Syos-1 <rene.azevedo.horst@gmail.com>
2014-10-14 13:07:28 +0200

    Code convention small changes and English

--- Spider/WebCrawler/WebCrawler.py
+++ Spider/WebCrawler/WebCrawler.py
@@ -1,7 +1,10 @@
 #WebCrawler Prototype
 #Rene van der Horst
 
-#Dit programma pakt data van de site http://www.alternate.nl
+#Indentation: 4 spaces
+#Variable naming: skeleton case
+
+#This program picks up data from alternate.nl
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,9 +13,8 @@ alternate_url = 'https://www.alternate.nl/html/highlights/page.html?hgid=286&tgi
 alternate_source_code = requests.get(alternate_url)
 
 plain_text = alternate_source_code.text
-
 soup = BeautifulSoup(plain_text)
-for link in soup.findAll('a',{'class':'h1x1'}):
+
+for link in soup.findAll('a', {'class': 'h1x1'}):
     href = link.get('href')
     print(href)
-
