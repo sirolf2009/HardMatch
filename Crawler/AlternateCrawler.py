ccedc53cd5dbc48b8d7464d2cb91066fc178b0a8
Syos-1 <rene.azevedo.horst@gmail.com>
2014-11-26 21:28:41 +0100

    Alternate Crawler improvements
    change of neo4j structure

--- Crawler/AlternateCrawler.py
+++ Crawler/AlternateCrawler.py
@@ -5,11 +5,6 @@ __license__ = "GPL"
 __version__ = "0.1"
 __status__ = "Development"
 
-#Indentation: 4 spaces
-#Variable naming: skeleton case
-
-#This program picks up data from alternate.nl
-
 import requests
 from bs4 import BeautifulSoup
 from py2neo import neo4j, Node, Relationship
@@ -73,17 +68,9 @@ def get_component_object(component_links):
                 component_mhz = component.text
                 break
 
-        #component_socket_type = component_soup.find_all('td', {'class': 'techDataSubCol techDataSubColValue'})
-        #component_clock_speed = component_soup.find_all('td', {'class': 'techDataSubCol techDataSubColValue'})
-
         component_brand_string = component_brand[0].text
         component_name_string = component_name[0].get('content')
         component_price_string = component_price[0].text
-        #component_socket_type_string = component_socket_type[0].get('content')
-        #component_clock_speed_string = component_clock_speed[0].get('content')
-        #print(component_name_string)
-        #print(component_price_string)
-        #print(component_socket_type_string)
 
         component = Component()
         Component.add_property(component, 'brand', component_brand_string)
@@ -91,41 +78,33 @@ def get_component_object(component_links):
         Component.add_property(component, 'price', component_price_string)
         Component.add_property(component, 'MHz', component_mhz)
         Component.save_component_with_relationships(component)
-
-
-#one = get_processor_links()
-#get_processor_data(one)
+        Component.print_property(component)
 
 
 class Component():
 
     properties = {}  # Dictionary
 
-    def add_property(self, key, value):
-        self.properties[key] = value
+    def add_property(self, key, property):
+        self.properties[key] = property
 
     def print_property(self):
-        for x in self.properties:
-            print(x)
-            print(self.properties[x])
+        for key in self.properties:
+            print("x: " + key)
+            print("p: " + self.properties[key])
 
     def save_component_with_relationships(self):
-        component = Node(brand=self.properties['brand'],
-                         name=self.properties['name'],
-                         price=self.properties['price'],
-                         mhz=self.properties['MHz']
-                         )
+        component = Node("component")
+        for key in self.properties:
+            component.properties[key] = self.properties[key]
         store.pull()
-        for i in self.properties:
-            component.properties[i] = self.properties[i]
-
-        relationship = Relationship(self, 'SOLD_AT', store, price=self.properties['price'])
+        relationship = Relationship(component, 'SOLD_AT', store, price=self.properties['price'])
         neo4j_db.create(component)
         neo4j_db.create(relationship)
 
 
 neo4j_db = neo4j.Graph("http://localhost:7474/db/data/")
-store = Node(type='Store', Name='alternate.nl')
+store = Node(type='Store', name='alternate.nl')
 neo4j_db.create(store)
 
 url = "http://www.alternate.nl"
@@ -133,3 +112,10 @@ hardware = get_hardware_page_url(url)
 components = get_component_page_url(hardware, url)
 links = get_component_links(components, url)
 get_component_object(links)
+
+
+#component_socket_type = component_soup.find_all('td', {'class': 'techDataSubCol techDataSubColValue'})
+#component_clock_speed = component_soup.find_all('td', {'class': 'techDataSubCol techDataSubColValue'})
+
+#component_socket_type_string = component_socket_type[0].get('content')
+#component_clock_speed_string = component_clock_speed[0].get('content')
