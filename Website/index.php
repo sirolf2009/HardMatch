f9bff535c20293da54d9a746b7b8026bdefb6959
dionn23 <dfengels@gmail.com>
2015-01-19 23:02:30 +0100

    Changed title for tests

--- Website/index.php
+++ Website/index.php
@@ -2,7 +2,7 @@
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <meta charset="utf-8">
-  <title>HardMatch - PC-Builder</title>
+  <title>HardMatch - PC-Builder Test</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
   <script src="js/jquery-1.11.1.min.js"></script>
